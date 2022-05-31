c341e773c4cd2866ca21cc44011435b2b50a838b
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-05-31 14:02:43 -0600

    added some changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: isProd ? 'production' : 'development',
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: 'src/index.html',
-    }),
-  ],
+    mode: isProd ? 'production' : 'development',
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ],
 };
